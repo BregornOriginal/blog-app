64eee33a40d9fa8b6c25fb8c00abb8bcc7b4ac5d
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-14 18:48:04 -0300

    create a new schema with a correct migrations
    - now author_id is going to exist at the respective tables

--- db/migrate/20220913161631_add_column_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :name, :string
-    add_column :users, :photo, :string
-    add_column :users, :bio, :text
-    add_column :users, :postscounter, :integer
-  end
-end
