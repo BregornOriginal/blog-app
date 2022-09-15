810ef755f2729cf627572efa43634fc87d456130
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-15 02:15:00 -0300

    Revert "add validation for post, to check title presence and max length(250)"
    This reverts commit f453ca41c54b2b80b833aa4e4b8181228d382878.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,4 @@
 class Post < ApplicationRecord
-  validates :title, presence: true
-  validates :title, length: { maximum: 250 }
-  # validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }
-  # validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }
-
   has_many :comments
   has_many :likes
   belongs_to :author, class_name: 'User', foreign_key: :author_id
