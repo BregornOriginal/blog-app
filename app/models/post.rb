a415b54216f62d511fadcebca236460679debd04
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-15 00:27:58 -0300

    add method to count post after save data

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,21 @@
 class Post < ApplicationRecord
   has_many :comments
   has_many :likes
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
     
+  # def self.update_post_counter(user)
+  #   user.posts_counter = Post.where(author: user).count
+  # end
+
+  after_save :update_post_counter
+
+  private
+
   def update_post_counter
-    Post.update(postscounter: posts.count)
+    author.increment!(:posts_counter)
+  end
+
+  def recent_comments
+    Comment.last(5)
   end
 end
