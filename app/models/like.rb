11840d226abc5e244f2072d898e1cb66dac590c1
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-15 00:27:33 -0300

    add method to counter likes after save data

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,16 @@
 class Like < ApplicationRecord
   belongs_to :post
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+
+  # def self.update_likes_counter(post)
+  #   post.likes_counter = Like.where(author: post).count
+  # end
+
+  after_save :update_likes_counter
+
+  private
+
+  def update_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
