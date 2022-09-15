337f26d8da73f6a6d5685cdaa9658042b20602c0
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-15 11:10:14 -0300

    add rspec

---
+++ test/models/comment_spec.rb
@@ -0,0 +1,30 @@
+# require 'rails_helper'
+
+# RSpec.describe Comment, :type => :model do
+#   it "is valid with valid attributes"
+#   it "is not valid without a title"
+#   it "is not valid without a description"
+#   it "is not valid without a start_date"
+#   it "is not valid without a end_date"
+# end
+
+
+
+# RSpec.describe Comment do
+#   subject { Comment.new(text: 'This is a text for testing the comment model') }
+
+#   before { subject.save }
+
+#   it 'text should be present' do
+#     subject.text = nil
+#     expect(subject).to_not be_valid
+#   end
+# end
+
+# require "test_helper"
+
+# class ArticleTest < ActiveSupport::TestCase
+#   # test "the truth" do
+#   #   assert true
+#   # end
+# end
