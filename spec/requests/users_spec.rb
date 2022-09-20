017db51e295abaaff80bbc25036446bdd40d0aba
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-20 14:32:25 -0300

    add test for users controllers

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -10,8 +10,15 @@ RSpec.describe 'Users', type: :request do
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/users/show'
+      get '/users'
       expect(response).to have_http_status(:success)
+    end
+  end
+  describe 'GET users' do
+    it 'render a template' do
+      get '/users'
+      expect(response).to render_template(:index)
+      expect(response.body).to include('Users list')
     end
   end
 end
