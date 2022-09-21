175bb55f31f98e26abc022ac0811e79a44215a0c
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-21 19:29:30 -0300

    refactor tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
   describe 'GET /id' do
     it 'returns http success' do
-      get '/users/id'
+      get '/users/2'
       expect(response).to have_http_status(:success)
     end
   end
