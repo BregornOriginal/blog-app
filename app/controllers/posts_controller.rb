b43b7aaf366ee6501dc8a8db7d371b71704b703d
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-21 17:34:41 -0300

    added method to show posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    # @post = Post.find(params[:author_id])
+    @posts = Post.where(author: @user)
+    @comments = Comment.where(author: @post)
+  end
 
   def show; end
 end
