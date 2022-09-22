c709c7ee5d51e691440ae899d68422f80de0e287
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-22 15:56:06 -0300

    create action for new and create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,40 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    # @post = Post.find(params[:author_id])
     @posts = Post.where(author: @user).order(id: :asc).limit(2)
   end
 
   def show
     @post = Post.find(params[:id])
     @comments = @post.comments
+  end
+
+  def new
+  post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: post } }
+    end
+  end
+
+  def create
+    # new object from params
+    post = Post.new(params.require(:post).permit(:author, :title, :text))
+    post.author = current_user
+    # respond_to block
+    respond_to do |format|
+      format.html do
+        if post.save
+          # success message
+          flash[:success] = "Post saved successfully"
+          # redirect to index
+          redirect_to users_url
+        else
+          # error message
+          flash.now[:error] = "Error: Post could not be saved"
+          # render new
+          render :new, locals: { post: post }
+        end
+      end
+    end
   end
 end
