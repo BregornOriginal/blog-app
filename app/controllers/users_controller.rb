80dd1301e235518f0288f2e02f31ad8a0b407390
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-22 09:34:07 -0300

    added a method to show three recent post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,10 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    # @user = User.find_by(id: params[:user_id])
   end
 
   def show
     @user = User.find(params[:id])
-    @posts = Post.where(author: @user).order(id: :asc).limit(3)
+    @posts = Post.where(author: @user)
   end
 end
