e1308148bd843ef86167c47f2f7dd82a2ef0a3ee
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-22 21:41:12 -0300

    added params to actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = Post.where(author: @user).order(id: :asc).limit(2)
+    @posts = Post.where(author: @user).order(id: :asc)
   end
 
   def show
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
     @comments = @post.comments
   end
@@ -17,22 +18,16 @@ class PostsController < ApplicationController
   end
 
   def create
-    # new object from params
     post = Post.new(params.require(:post).permit(:author, :title, :text))
     post.author = current_user
-    # respond_to block
     respond_to do |format|
       format.html do
         if post.save
-          # success message
           flash[:success] = "Post saved successfully"
-          # redirect to index
           redirect_to users_url
         else
-          # error message
           flash.now[:error] = "Error: Post could not be saved"
-          # render new
-          render :new, locals: { post: post }
+          render :new, status: 422
         end
       end
     end
