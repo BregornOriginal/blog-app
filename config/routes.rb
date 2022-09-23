ca9b6e5e95ca1e3ffc4fd0c8d948205937399809
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-22 21:43:58 -0300

    change routes for comments resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :create, :show] do
-      resources :comments, only: [:new]
+      resources :comments, only: [:create]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
