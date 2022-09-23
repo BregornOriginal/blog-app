4b2a1887d692f089dceac113066292fcffb268a7
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-22 21:44:13 -0300

    added action to comments controlers for new and create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,30 @@
 class CommentsController < ApplicationController
   def new
+    @user = current_user
+    @post = Post.find(params[:post_id])
+    comment = Comment.new
+      respond_to do |format|
+        format.html { render :new, locals: { comment: comment } }
+      end
+    end
+    
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    comment = Comment.new(params.require(:comment).permit(:text))
+    comment.author = current_user
+    comment.post_id = @post.id
+
+    respond_to do |format|
+      format.html do
+        if comment.save
+          flash[:success] = "Comment saved successfully"
+          redirect_to user_post_url(@user.id, @post.id)
+        else
+          flash.now[:error] = "Error: Post could not be saved"
+          render :new, locals: { comment: comment }
+        end
+      end
+    end
   end
 end
