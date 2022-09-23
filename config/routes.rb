d18b1d63564ed5de93f09a33971248caeaa34d18
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-22 23:03:48 -0300

    add likes controllers with actions for each post

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :create, :show] do
-      resources :comments, only: [:create]
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
