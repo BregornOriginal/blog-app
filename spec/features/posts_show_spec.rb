6267c9457de577ea290b3338360dc0689ac9d951
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-26 23:04:50 -0300

    fix linter errors

--- spec/features/posts_show_spec.rb
+++ spec/features/posts_show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'show page', type: :feature do
   before(:each) do
     @user = User.create(name: 'Julio', bio: 'This is the bio',
-    photo: "https://i.im.ge/2022/09/22/1Ud6MY.druid.th.png")
+                        photo: 'https://i.im.ge/2022/09/22/1Ud6MY.druid.th.png')
     @id = @user.id
   end
 
@@ -11,7 +11,8 @@ RSpec.describe 'show page', type: :feature do
     before(:each) do
       @post = Post.create(title: 'My first post', author: @user, text: 'Text of my first post')
       @post_id = @post.id
-      @comment = Comment.create(post: @post, author: @user, text: 'Restoration is an excelent option when you have to go to raids!')
+      @comment = Comment.create(post: @post, author: @user,
+                                text: 'Restoration is an excelent option when you have to go to raids!')
     end
 
     it 'show a post title' do
@@ -28,3 +29,5 @@ RSpec.describe 'show page', type: :feature do
       visit "users/#{@id}/posts/#{@post_id}"
       expect(page).to have_content(@post.comments_counter)
     end
+  end
+end
