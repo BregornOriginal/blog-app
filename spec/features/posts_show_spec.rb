1dc842370dc5f66eb719b943392361dea0439eb6
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-27 09:45:01 -0300

    added missing test on post show page

--- spec/features/posts_show_spec.rb
+++ spec/features/posts_show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'show page', type: :feature do
   before(:each) do
     @user = User.create(name: 'Julio', bio: 'This is the bio',
-                        photo: 'https://i.im.ge/2022/09/22/1Ud6MY.druid.th.png')
+    photo: "https://i.im.ge/2022/09/22/1Ud6MY.druid.th.png")
     @id = @user.id
   end
 
@@ -11,8 +11,7 @@ RSpec.describe 'show page', type: :feature do
     before(:each) do
       @post = Post.create(title: 'My first post', author: @user, text: 'Text of my first post')
       @post_id = @post.id
-      @comment = Comment.create(post: @post, author: @user,
-                                text: 'Restoration is an excelent option when you have to go to raids!')
+      @comment = Comment.create(post: @post, author: @user, text: 'Restoration is an excelent option when you have to go to raids!')
     end
 
     it 'show a post title' do
@@ -28,6 +27,26 @@ RSpec.describe 'show page', type: :feature do
     it 'shows the amounts of comments' do
       visit "users/#{@id}/posts/#{@post_id}"
       expect(page).to have_content(@post.comments_counter)
+    end
+
+    it 'shows the amounts of likes' do
+      visit "users/#{@id}/posts/#{@post_id}"
+      expect(page).to have_content(@post.likes_counter)
+    end
+
+    it 'shows post body' do
+      visit "users/#{@id}/posts/#{@post_id}"
+      expect(page).to have_content("Post")
+    end 
+
+    it 'shows the username of each commentor' do
+      visit "users/#{@id}/posts/#{@post_id}"
+      expect(page).to have_content("#{@comment.author.name}")
+    end
+
+    it 'shows the comment of each commentor' do
+      visit "users/#{@id}/posts/#{@post_id}"
+      expect(page).to have_content("#{@user[@comment]}")
     end
   end
 end
