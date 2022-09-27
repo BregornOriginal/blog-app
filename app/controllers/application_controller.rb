561bd4aceae9b8334e18205320797068b2b761b0
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-27 19:44:32 -0300

    added configuration for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    User.first
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
 end
