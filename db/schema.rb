b11ad0ff1de70666f91b8f9c5f56aab3357aad4b
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-28 17:18:32 -0300

    set a photo user by default

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_27_205151) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_28_201149) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,7 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_27_205151) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "photo"
+    t.string "photo", default: "https://lanmo.unam.mx/repositorio/LANMO/rn/hablantes/imagenes/directorio/hablante_blank.png"
     t.string "bio"
     t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
