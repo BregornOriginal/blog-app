b7958e4cd56e8569f2b82e6ae15cfd0df1a45308
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-28 20:01:00 -0300

    update comment model to update counter when destroy

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   after_save :update_comments_counter
-
+  after_destroy :update_comments_counter
+  
   private
 
   def update_comments_counter
