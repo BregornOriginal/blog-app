9b96b98a0299d76b0af5b98dda86c0a0765eaa1d
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-28 20:05:42 -0300

    update like model to a user can add only one like per post

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,8 +3,9 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   after_save :update_likes_counter
-
-  # validates :author, uniqueness: {scope: :post}
+  after_destroy :update_likes_counter
+  
+  validates :author, uniqueness: {scope: :post}
 
   private
 
