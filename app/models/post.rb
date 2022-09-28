4ed1339ff4bfe25bd7dc9c2937fbd7b7cc223b5a
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-28 20:00:47 -0300

    update post model to update counter when destroy

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,14 @@
 class Post < ApplicationRecord
-  validates :title, presence: true
-  validates :title, length: { maximum: 250 }
+  validates :title, presence: true, length: { minimum: 1, maximum: 250 }
   validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }
   validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }
 
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   belongs_to :author, class_name: 'User', foreign_key: :author_id
 
   after_save :update_post_counter
+  after_destroy :update_post_counter
 
   def recent_comments
     comments.last(5)
