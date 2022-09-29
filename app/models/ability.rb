92ea98d09c164ba19f1ac1239d8b1f752c2762b9
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-29 00:08:20 -0300

    refactor ability to give permision to user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,9 +2,15 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :manage, :all if user.role == 'admin'
 
-    can :delete_post, Post, author: user
-    can :delete_comment, Comment, author: user
+    return unless user.present?
+
+    can :read, :all
+    can :manage, Post, author: user
+    can :manage, Comment, author: user
+
+    return unless user.role == 'admin'
+
+    can :manage, :all
   end
 end
