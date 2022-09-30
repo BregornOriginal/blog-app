1e751ac89cd636ba0dca2b8f00d61efebc1034eb
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-30 00:08:56 -0300

    Revert "added gem jwt and authentication controllers"
    This reverts commit 45721781941a1052ed82bab374ec0f941687f20b.
    
    Restore rspec of requests

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  before(:each) do
+    @user = User.create(name: 'Julio')
+    @id = @user.id
+  end
+
+  describe 'GET /users/:id/posts/' do
+    before(:example) { get "/users/#{@id}/posts/" }
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct placeholder text in its body' do
+      expect(response.body).to include('<h1>Posts</h1>')
+    end
+  end
+
+  describe 'GET /users/:id/posts/:id/' do
+    before(:example) do
+      post = Post.create(title: 'My first post', author: @user, text: 'Text of my first post')
+      get "/users/#{@id}/posts/#{post.id}/"
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct placeholder text in its body' do
+      expect(response.body).to include("<section class='post-container'>")
+    end
+  end
+end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET /' do
+    before(:example) { get '/' }
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes the correct placeholder text in its body' do
+      expect(response.body).to include('<h1>Users</h1>')
+    end
+  end
+
+  describe 'GET users/:id/' do
+    before(:example) do
+      user = User.create(name: 'Julio')
+      get "/users/#{user.id}/"
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct placeholder text in its body' do
+      expect(response.body).to include("<div class='user-info'>")
+    end
+  end
+end
