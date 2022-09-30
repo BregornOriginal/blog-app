50bae150ed1091b69a412d786221865da6015e62
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-30 00:11:22 -0300

    Revert "added gem jwt and authentication controllers"
    This reverts commit 45721781941a1052ed82bab374ec0f941687f20b.

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,8 +1,5 @@
 class Api::V1::CommentsController < ApplicationController
-  include JsonWebToken
-  skip_before_action :verify_authenticity_token
-  before_action :authenticate_request
-  
+
   # GET api/v1/users/1/posts/1/comments
   def index
     @post = Post.find(params[:post_id])
@@ -11,23 +8,11 @@ class Api::V1::CommentsController < ApplicationController
     render json: @comments
   end
 
-  # POST api/v1/users/1/posts/1/comments
   def create
-    @post = Post.find(params[:post_id])
-    @comment = Comment.new(comment_params)
-    @comment.author = @current_user
-    @comment.post = @post
+    @comment = Comment.create(text: 'New Comment',
+      author: User.find(1), post: Post.find(1))
 
-    if @comment.save
-      render json: @comment, status: :created
-    else
-      render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
-    end
-    
-    private
+    render json: @comment
 
-    def comment_params
-      params.require(:comment).permit(:text)
-    end
   end
 end
