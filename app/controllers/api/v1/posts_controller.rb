b2873cda6c4d24c9b315754419aa78affad54fca
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-30 10:53:30 -0300

    fix linter errors

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::PostsController < ApplicationController
-
   # GET api/v1/users/1/posts/
 
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
-  
-    render json: @posts 
+
+    render json: @posts
   end
 end
