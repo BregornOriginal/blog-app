5daadac1bf7fe133e8166f3078bc2540b78cb7b7
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-09-30 14:28:47 -0300

    create api-documentation for comments request

---
+++ spec/requests/api/comments_spec.rb
@@ -0,0 +1,44 @@
+require 'swagger_helper'
+
+RSpec.describe 'Blogs API' do
+
+  path '/api/v1/users/{id}/posts/{id}/comments' do
+
+    get 'Retrieves comments of a post of the user' do
+      tags 'Comments'
+      produces 'application/json'
+      parameter name: 'user_id', in: :path, type: :integer, description: 'user_id'
+      parameter name: 'post_id', in: :path, type: :integer, description: 'post_id'
+
+      response '200', 'Comment found' do
+        let(:user_id) { '123' }
+        let(:post_id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('create comment') do
+      response(200, 'successful') do
+        let(:user_id) { '123' }
+        let(:post_id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
